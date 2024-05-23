7dd501bce9edcc3e3d9e5b314dd3a8ce609a87d1
ASPLSomnath <somnathpy.adansa@gmail.com>
2024-05-23 13:40:30 +0530

    ok

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template('home.html')
+#
+@app.route('/login')
+def login_1():
+	return render_template('register.html')
 
 if __name__ == "__main__":
 	app.run()
