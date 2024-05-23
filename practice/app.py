1084837aead7081c887a28424da90c63dabf98b6
ASPLSomnath <somnathpy.adansa@gmail.com>
2024-05-23 13:49:26 +0530

    abcd

--- practice/app.py
+++ practice/app.py
@@ -2,3 +2,16 @@ from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
+@app.route('/')
+def name():
+	return render_template('som.html')
+
+@app.route('/place')
+def place():
+	return render_template('place.html')
+
+
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
